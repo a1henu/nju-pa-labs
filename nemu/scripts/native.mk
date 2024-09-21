56fa4d944a10c11b7eb569998bfabee3d21ecb00
tracer-ics2024 <tracer@njuics.org>
2024-09-21 12:30:46 +0800

    >  compile NEMU 1234567 test Linux A1heNu 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:30:46 up  2:26,  1 user,  load average: 0.07, 0.12, 0.09

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -34,7 +34,7 @@ NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
-	@echo $(DIFF_REF_SO)
+	@echo $(NEMU_EXEC)
 	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
 
